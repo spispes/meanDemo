388ae3baf1e50fdc60ca991ec4881ec5d5582eae
spispes <spispes@gmail.com>
2015-10-01 15:17:54 +0200

    db init

--- server.js
+++ server.js
@@ -3,11 +3,10 @@
  */
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var mongoose;
-var express;
-mongoose = require("./config/mongoose");
-express = require("./config/express");
+var mongoose = require("./config/mongoose");
+var express = require("./config/express");
 
+var db = mongoose();
 var app = express();
 
 app.listen(3000);
