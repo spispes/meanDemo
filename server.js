7c947fca5c1254f9bf2a002a56b6c8b3b130cffa
spisakp <peter.spisak@telefonica.com>
2015-10-01 14:28:12 +0200

    mongoose needs to be the very first module to be loaded

--- server.js
+++ server.js
@@ -3,8 +3,11 @@
  */
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+var mongoose;
 var express;
+mongoose = require("./config/mongoose");
 express = require("./config/express");
+
 var app = express();
 
 app.listen(3000);
