a3287a5729050179c98d4a8aa15c777a64ee2374
spispes <spispes@gmail.com>
2015-10-01 14:51:47 +0200

    post for users added

---
+++ app/routes/users.server.routes.js
@@ -0,0 +1,8 @@
+/**
+ * Created by pes on 01.10.15.
+ */
+var users = require('../../app/controllers/users.server.controller');
+
+module.exports = function(app) {
+    app.route('/users').post(users.create);
+};
