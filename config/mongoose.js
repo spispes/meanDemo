c80099b9ce0dc9903da20cddb30f1a742d98c0ff
spispes <spispes@gmail.com>
2015-10-01 14:25:58 +0200

    Register User Schema

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,16 @@
  */
 var config = require('./config'),
     mongoose = require('mongoose');
+/**
+ *
+ * @returns {Mongoose}
+ */
 module.exports = function() {
     var db = mongoose.connect(config.db);
+
+
+     //define schemas
+    require('../app/models/user.server.model');
+
     return db;
 };
